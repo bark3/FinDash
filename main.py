a3a53a8178cebd53e7fb9817a86668d89181c9d2
bark3 <ztb0003@gmail.com>
2021-02-16 19:05:23 -0500

    Clean up header

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 # Import libraries
 from __future__ import print_function
 from selenium import webdriver
